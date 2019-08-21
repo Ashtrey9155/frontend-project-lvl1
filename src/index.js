5272b38b9a3a9d639a60a21921be67ac6dbe926b
System Administrator <root@M-Bellerophon.local>
2019-08-21 19:14:14 +0300

    add readme travis_badje

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import readlineSync from 'readline-sync';
 
 const actual = () => {
-	const name = readlineSync.question('May I have your name? ');
-	console.log("Hello, " + name + "!");
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
 
 export default actual;
